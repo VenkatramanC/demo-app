85f7b6a35dc3be64628b8635249e525428ab9e21
Venkatraman C <venkatraman.c9@gmail.com>
2016-09-07 16:42:22 +0530

    Hero class exported and hero object added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}} {{hero}}</h1>'
+    template: '<h1>{{title}} {{hero.name}}</h1>'
 })
+
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero = 'Sachin Tendulkar';
+	hero: Hero{
+		id: 1;
+		name: 'Wind Strom';
+	}	
 }
+
+

61c77b808825da77a4141266c59f031ecb61bb0f
Venkatraman C <venkatraman.c9@gmail.com>
2016-09-07 17:17:04 +0530

    Form module added to the ng modul and ng-model added in view for two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,15 +7,22 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}} {{hero.name}}</h1>'
+    template:`
+  		<h1>{{title}}</h1>
+  		<h2>{{hero.name}} details!</h2>
+  		<div><label>id: </label>{{hero.id}}</div>
+  		<div><label>name: </label>
+  		<input [(ngModel)]="hero.name" placeholder="name"></div>
+  `
+
 })
 
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero: Hero{
-		id: 1;
-		name: 'Wind Strom';
-	}	
+	hero: Hero = {
+		id: 1,
+		name: 'Windstrom'
+	};		
 }
 
 
